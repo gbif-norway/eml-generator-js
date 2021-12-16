4da8d62a03f5dafafa3687d6f2ce11f452876e59
rukayaj@gmail.com <rukayaj@gmail.com>
2021-12-16 19:50:18 +0100

    Remove old function

--- src/MakePopulatedEML.tsx
+++ src/MakePopulatedEML.tsx
@@ -36,45 +36,7 @@ const MakePopulatedEML = (jsonformsData) => {
     console.log('starting');
     Seek(fieldKey, fieldValue, emlDoc.querySelector('eml'));
   }
-
-  /*
-  var datasetNode = emlDoc.querySelector('eml>dataset');
-  console.log(jsonformsData);
-
-  for (const [fieldKey, fieldValue] of Object.entries(jsonformsData)) {
-    var node = emlDoc.querySelector('eml ' + fieldKey);
-    if (Array.isArray(fieldValue)) {
-      // There may be multiple e.g. creator nodes in the eml. The blank template
-      // has 1 single example for each of these. So we keep an empty clone
-      // of it here, and make more clones from this one if required, as later on it will get populated
-      var nodeTemplate = node!.cloneNode(true);
-
-      for (var [k, v] of Object.entries(fieldValue!)) {
-        // See previous comment; Creator 1 uses the already existing node, creator 2, 3, 4, etc needs a clone
-        if (k != '0') {
-          node = (nodeTemplate.cloneNode(true) as HTMLElement);
-          datasetNode!.appendChild(node);
-        }
-
-        // Fill in creator.givenName, etc
-        for (var [label, item] of Object.entries(v)) {
-          var subNode = node!.querySelector(label);
-          if (subNode) { subNode.innerHTML = String(item) || ''; }
-        }
-      }
-    }
-    else if(typeof fieldValue == 'object' && fieldValue != null) {
-      // This is for nested objects like <coverage><geographicCoverage>
-      for (const [label, item] of Object.entries(fieldValue)) {
-        var subNode = node!.querySelector(label);
-        if (subNode) { subNode.innerHTML = String(item) || ''; }
-      }
-    }
-    else {
-      node!.innerHTML = String(fieldValue) || '';
-    }
-  }*/
-
+  
   var XMLS = new XMLSerializer();
   var inp_xmls = XMLS.serializeToString(emlDoc);
   console.log(inp_xmls);
